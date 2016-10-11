e56154b7dd16b00446c2e3e8d91db79d375b2218
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-10-11 09:26:12 +0200

    Install grunt-contrib-imagemin.

--- gruntfile.js
+++ gruntfile.js
@@ -1,9 +1,11 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+
+	var mozjpeg = require('imagemin-mozjpeg');
 
 	grunt.initConfig({
 		clean: {
-	  		css: ['build/css/*'],
-	  		js: ['build/js/*'],
+			css: ['build/css/*'],
+			js: ['build/js/*'],
 		},
 
 		uglify: {
@@ -16,11 +18,11 @@ module.exports = function(grunt) {
 					screwIE8: true,
 				},
 				files: {
-						'build/js/app.min.js': [
-							'assets/js/footbel.js',
-							'assets/js/instagram.js',
-							'assets/js/twitter.js',
-							'assets/js/kcvv.js',
+					'build/js/app.min.js': [
+						'assets/js/footbel.js',
+						'assets/js/instagram.js',
+						'assets/js/twitter.js',
+						'assets/js/kcvv.js',
 					]
 				}
 			},
@@ -33,61 +35,81 @@ module.exports = function(grunt) {
 					screwIE8: true,
 				},
 				files: {
-						'build/js/app.min.js': [
-							'assets/js/footbel.js',
-							'assets/js/instagram.js',
-							'assets/js/twitter.js',
-							'assets/js/kcvv.js',
+					'build/js/app.min.js': [
+						'assets/js/footbel.js',
+						'assets/js/instagram.js',
+						'assets/js/twitter.js',
+						'assets/js/kcvv.js',
 					]
 				}
-		  	}	
+			}
 		},
 		sass: {
-		  dev: {
-		  	options: {
-			   style: 'expanded',
-			   compass: true,
-			   sourcemap: 'auto',
-			   debugInfo: true,
-			   lineNumbers: true,
+			dev: {
+				options: {
+					style: 'expanded',
+					compass: true,
+					sourcemap: 'auto',
+					debugInfo: true,
+					lineNumbers: true,
+				},
+				files: {
+					'build/css/style.css': 'assets/sass/style.scss'
+				}
 			},
-		    files: {
-		        'build/css/style.css': 'assets/sass/style.scss'
-		    }
-		  },
-		  dist: {
-		  	options: {
-			   style: 'compressed',
-			   compass: true,
-			   sourcemap: 'none',
-			},
-		    files: {
-		        'build/css/style.css': 'assets/sass/style.scss'
-		    }
-		  }
+			dist: {
+				options: {
+					style: 'compressed',
+					compass: true,
+					sourcemap: 'none',
+				},
+				files: {
+					'build/css/style.css': 'assets/sass/style.scss'
+				}
+			}
+		},
+		imagemin: {
+			img: {
+				options: {
+					optimizationLevel: 7,
+					svgoPlugins: [{ removeViewBox: false }],
+					use: [mozjpeg()]
+				},
+				files: [{
+					expand: true,
+					cwd: 'assets/images',
+					src: ['**/*.{png,jpg,gif,svg}'],
+					dest: 'build/images'
+				}]
+			}
 		},
 		watch: {
 			js: {
 				files: ['assets/js/**/*.js'],
-		    	tasks: ['uglify:dev'],
+				tasks: ['uglify:dev'],
 			},
 			sass: {
 				files: ['assets/sass/**/*.scss'],
 				tasks: ['sass:dev'],
 			},
+			imagemin: {
+				files: ['assets/images/**/*.{png,jpg,gif,svg}'],
+				tasks: ['imagemin'],
+			}
 		}
 	});
 
 	// RUN BY DEFAULT
 	grunt.registerTask('default', ['build:dev', 'watch']);
 
-	grunt.registerTask('build:dist', ['clean', 'sass:dist', 'uglify:dist']);
-	grunt.registerTask('build:dev', ['clean', 'sass:dev', 'uglify:dev']);
+	grunt.registerTask('build:dist', ['clean', 'sass:dist', 'uglify:dist', 'imagemin']);
+	grunt.registerTask('build:dev', ['clean', 'sass:dev', 'uglify:dev', 'imagemin']);
 
 	// RUN TASKS
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-compass');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 };
